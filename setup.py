5329777eef263eecfa90460ca46218a2f808a2d6
Rishi Gurnani <rgurnani96@gmail.com>
2021-08-12 17:48:57 -0400

    Added LICENSE and setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='shared',
+      version='0.0.1',
+      description='',
+      author='Rishi Gurnani',
+      author_email='rgurnani96@gatech.edu',
+      packages=find_packages(where=["nndebugger"]),
+      license='LICENSE.txt',
+    )
