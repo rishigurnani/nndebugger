4225cc4ec64f599889d52dc05a76cfe3089aa63c
Rishi Gurnani <rgurnani96@gmail.com>
2021-12-12 13:10:11 -0500

    Added test_output_shape_pass

--- tests/test_nndebugger.py
+++ tests/test_nndebugger.py
@@ -1,5 +1,87 @@
 import pytest
+# standard libraries
+import pandas as pd
+import numpy as np
+from rdkit import Chem
+from rdkit.Chem import AllChem
+from torch import tensor, cuda, manual_seed, zeros, nn, optim
+from torch import float as torch_float
+from torch_geometric.loader import DataLoader
+from torch import device as torch_device
+import random
+from sklearn.model_selection import train_test_split
+from torch_geometric.data import Data
+
+# nndebugger functions
+from bin.nndebugger import constants, loss, dl_debug
+from bin.nndebugger import torch_utils as utils
 from bin.nndebugger import __version__
+from .utils_test import featurize_smiles, MyNet
+
+# set seeds for reproducibility
+random.seed(constants.RANDOM_SEED)
+manual_seed(constants.RANDOM_SEED)
+np.random.seed(constants.RANDOM_SEED)
 
 def test_version():
     assert __version__ == "0.1.0"
+
+@pytest.fixture
+def example_data():
+    # load data
+    data_df = pd.read_csv('data/export.csv',index_col=0)
+    # featurize data set
+    n_features = 512
+    n_data = len(data_df)
+    feature_array = np.zeros((n_data, n_features))
+    ind = 0
+    for smiles in data_df.smiles.values:
+        feature_array[ind,:] = featurize_smiles(smiles, n_features)
+        ind += 1
+    # prepare inputs for DebugSession
+    # bug free processing pipeline!
+    # data_set
+    n_test = int(np.floor(n_data*constants.TRAIN_FRAC))
+    n_train = n_data - n_test
+    (X_train, X_test, label_train, 
+    label_test) = train_test_split(
+                                        feature_array,
+                                        data_df.value.values.tolist(),
+                                        test_size=n_test,
+                                        shuffle=True,
+                                        random_state=constants.RANDOM_SEED
+                                    )
+
+    train_X = [Data(x=tensor(X_train[ind,:], dtype=torch_float).view(1,n_features),
+                    y=tensor(label_train[ind], dtype=torch_float)
+                ) 
+                for ind in range(n_train)]
+    loss_fn = loss.st_loss()
+    capacity_ls = [1,2,3]
+    return {
+        'correct_model_class_ls': [lambda : MyNet(n_features, 1, capacity) for capacity in
+            capacity_ls
+        ], # a list of models that are bug free!
+        'model_type': 'mlp',
+        'capacity_ls': capacity_ls,
+        'data_set': train_X,
+        'zero_data_set': [Data(x=zeros((1,n_features)), y=x.y) for x in train_X],
+        'loss_fn': loss_fn,
+        'device': torch_device('cuda' if cuda.is_available() else 'cpu'),
+    }
+
+def test_output_shape_pass(example_data):
+    '''
+    This test should pass since it uses a bug-free model
+    '''
+    ds = dl_debug.DebugSession(
+        example_data['correct_model_class_ls'], 
+        example_data['model_type'], 
+        example_data['capacity_ls'], 
+        example_data['data_set'], 
+        example_data['zero_data_set'], 
+        example_data['loss_fn'],
+        example_data['device'], 
+        do_test_output_shape=True
+    )
+    ds.main()
---
+++ tests/utils_test.py
@@ -0,0 +1,47 @@
+import numpy as np
+from rdkit import Chem
+from rdkit.Chem import AllChem
+from torch import nn
+
+from bin.nndebugger import torch_utils as utils
+
+def featurize_smiles(smile, n_features):
+    smile = smile.replace('*', 'H')
+    mol = Chem.MolFromSmiles(smile)
+    fp = AllChem.GetMorganFingerprintAsBitVect(mol, radius=2, nBits=n_features, useChirality=True)
+    return np.array(fp)
+
+# a logical architecture that will pass all cases
+class MyNet(nn.Module):
+    
+    def __init__(self, input_dim, output_dim, capacity):
+
+        super(MyNet,self).__init__()
+        self.layers = nn.ModuleList()
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        self.n_hidden = capacity
+        unit_sequence = utils.unit_sequence(self.input_dim, 
+                                            self.output_dim, 
+                                            self.n_hidden)
+        self.relu = nn.ReLU()
+        # set up hidden layers
+        for ind,n_units in enumerate(unit_sequence[:-2]):
+            size_out_ = unit_sequence[ind+1]
+            layer = nn.Linear(n_units, size_out_)
+            self.layers.append(layer)
+
+        # set up output layer
+        size_in_ = unit_sequence[-2]
+        size_out_ = unit_sequence[-1]
+        layer = nn.Linear(size_in_, size_out_)
+        self.layers.append(layer)
+    
+    def forward(self, data):
+        x = data.x
+        for i in range(len(self.layers)):
+            x = self.layers[i](x)
+            if i < (self.n_hidden - 1):
+                x = self.relu(x)
+   
+        return x.view(data.num_graphs,)
